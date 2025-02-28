6e0c7b81b3375bdf7667938f528d91c9cb5f13d5
bevithh <166908855+bevithh@users.noreply.github.com>
2025-02-28 20:46:11 +1100

    Add files via upload

---
+++ Beach Travel Guide/index.php
@@ -0,0 +1,392 @@
+<?php include 'header.inc.php'; ?>
+include 'quiztable.php';
+
+// Call the setup function
+quiztable();
+?>
+<body>
+  <p>Click the link below to watch the YouTube video:</p>
+    <a href="https://youtu.be/njqsJxligvE" target="_blank">Watch the YouTube video</a>
+  <nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="topic.html">Topic</a></li>
+        <li><a href="quiz.html">Quiz</a></li>
+        <!-- Add more links as needed -->
+        <li><a href="enhancements.html">Enhancements</a></li>
+        <li><a href="https://youtu.be/eRvVAF8XqaQ" target="_blank">Watch the YouTube video 2</a></li>
+    </ul>
+</nav>
+    <!-- Background -->
+    <div class="bg">
+      <figure>
+        <img src="blossom/images/bg.jpg" alt="Background Image">
+        <figcaption>background image</figcaption>
+    </figure>
+    <!--header--->
+    <header>
+        <a href="#" class="logo">Blossom</a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#package">Package</a></li>
+            <li><a href="#destination">Destination</a></li>
+            <li><a href="#accommodation">Accommodations</a></li>
+            <li><a href="#tips">Travel tips</a></li>
+        </ul>
+    </header>
+    <!--Home section--->
+    <section class="home" id="home">
+        <div class="home-text">
+            <h1>Blossom <br> Your Beach</h1>
+            <p>Get yourself an access to the most exclusive beach tips and tricks for your next beach trip! <br> Let us help you blossom up your trip.</p>
+            <a href="#" class="home-btn">Let's go now</a>
+        </div>
+    </section>
+    <br /><br /><br />
+    
+    <!--Package section-->
+
+<section class="package" id="package">
+        <div class="title">
+          <h2>Our Beach <br> Tour Packages</h2>
+        </div>
+        
+        <div class="package content">
+            <!-- St Kilda Beach Package -->
+          <div class="box">
+            <div class="thum">
+              <img src="images/kilda.png">
+              <h3>$135 per pax [1 day]</h3>
+            </div>
+        
+            <div class="detail">
+              <div class="location">
+                <h4>St. Kilda Beach</h4>
+                <p>20 minutes from CBD Melbourne</p>
+              </div>
+            
+        
+              <div class="star">
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+              </div>
+            </div>
+          </div>
+           <!-- Brighton Beach Package -->
+          <div class="box">
+            <div class="thum">
+              <img src="images/brighton.png">
+              <h3>$125 per pax [1 day]</h3>
+            </div>
+        
+            <div class="detail">
+              <div class="location">
+                <h4>Brighton Beach</h4>
+                <p>34 minutes from CBD Melbourne</p>
+              </div>
+            
+        
+              <div class="star">
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+              </div>
+            </div>
+          </div>
+           <!-- Altona Beach Package -->
+          <div class="box">
+            <div class="thum">
+              <img src="images/altona.png">
+              <h3>$115 per pax [1 day]</h3>
+            </div>
+        
+            <div class="detail">
+              <div class="location">
+                <h4>Altona Beach</h4>
+                <p>34 minutes from CBD Melbourne</p>
+              </div>
+            
+        
+              <div class="star">
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+              </div>
+            </div>
+          </div>
+            <!-- Williamstown Beach Package -->
+          <div class="box">
+            <div class="thum">
+              <img src="images/williams.png">
+              <h3>$165 per pax [1 day]</h3>
+            </div>
+        
+            <div class="detail">
+              <div class="location">
+                <h4>Williamstown Beach</h4>
+                <p>30 minutes from CBD Melbourne</p>
+              </div>
+            
+        
+              <div class="star">
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+                 <a href="#"><i class='bx bxs-star'></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+</section>
+ 
+
+            
+
+    <!-- Destination section -->
+    <section>
+        <h1 align="center">Popular Places near Beaches for tourists to visit as well!</h1>
+        <br>
+        <h2 align="center"> 
+            <a href="https://www.google.com.au/maps/dir//St+Kilda+beach,+Victoria/@-37.8681193,144.9676548,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad668634ecee1b7:0x917edcf1e3399bda!2m2!1d144.9740049!2d-37.8678765?entry=ttu">Near St. Kilda Beach</a>
+        </h2>
+        <br /><br /><br />
+        <h3 align="center">
+            Royal Botanic Garden <a href="https://www.google.com.au/maps/dir//Royal+Botanic+Gardens+Victoria+-+Melbourne+Gardens,+Melbourne+VIC+3004/@-37.8302443,144.9775747,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad64296fb298d3b:0xf0456760531d7c0!2m2!1d144.9801496!2d-37.8302443?entry=ttu">(Map!)</a>
+        </h3>
+        <img src="images/RoyalBotanicGarden.png" class="center-img">
+        <br /><br /><br />
+    
+        <h2 align="center">
+            <a href= "https://www.google.com.au/maps/dir//Brighton+beach,+Victoria/@-37.9176957,144.9809155,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad66f344bd2f09f:0x2e59e244a2cf745f!2m2!1d144.9861397!2d-37.9159683?entry=ttu">Near Brighton Beach</a>
+        </h2>
+        <br /><br /><br />
+
+        <h3 align="center">
+            Billilla Mansion <a href="https://www.google.com.au/maps/dir//Billilla+Mansion,+26+Halifax+St,+Brighton+VIC+3186/@-37.9178023,144.9986281,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad669ac2b5c092f:0xf2424cd964134e04!2m2!1d145.001203!2d-37.9178023?entry=ttu">(Map!)</a>
+        </h3>
+        <img src="images/Billilla.png" class="center-img">
+        <br /><br /><br />
+
+        <h3 align="center">
+            Skydive Melbourne <a href="https://www.google.com.au/maps/dir//Skydive+Melbourne,+42A+Marine+Parade,+St+Kilda+VIC+3182/@-37.9178023,144.9986281,17z/data=!3m1!5s0x6ad66889ea2dd817:0xb7a4b863fa848fe5!4m8!4m7!1m0!1m5!1m1!1s0x6ad668899918479d:0xa67bde6f99086b64!2m2!1d144.9764416!2d-37.8741875?entry=ttu">(Map!)</a>
+        </h3>
+        <img src="images/Skydive.png" class="center-img">
+        <br /><br /><br />
+
+        <h2 align="center">
+            <a href="https://www.google.com.au/maps/dir//Williamstown+beach,+Victoria+3016/@-37.8678526,144.8889617,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad666bc25d430b9:0x1974dca53cccc54a!2m2!1d144.89083!2d-37.8672192?entry=ttu">Near Williamstown Beach</a>
+        </h2>
+        <br /><br /><br />
+
+        <h3 align="center">
+            Williamstown Botanic Garden <a href="https://www.google.com.au/maps/dir//Williamstown+Botanic+Gardens,+Williamstown+Botanical+Gardens,+Giffard+St+%26,+Osborne+St,+Williamstown+VIC+3016/@-37.8677607,144.894281,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad666950827c09b:0xf04567605311e40!2m2!1d144.8968685!2d-37.8677138?entry=ttu">(Map!)</a>
+        </h3>
+        <img src="images/Williamstown.png" class="center-img">
+        <br /><br /><br />
+
+        <h3 align="center">
+           Hobsons Bay visitor information centre <a href="https://www.google.com.au/maps/dir//Hobsons+Bay+Visitor+Information+Centre,+1+Syme+St,+Williamstown+VIC+3016/@-37.8628626,144.9022165,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad666ee105d4bb9:0xc5d4cd84355bdbc7!2m2!1d144.90477!2d-37.8629084?entry=ttu">(Map!)</a>
+        </h3>
+        <img src="images/Hobsons.png" class="center-img">
+        <br /><br /><br />
+
+        <h3 align="center">
+            Railway Museum <a href="https://www.google.com.au/maps/dir//Newport+Railway+Museum,+26+Champion+Rd,+Newport+VIC+3015/@-37.8562129,144.8841838,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad666c5cfee3d77:0xe2239b8fb3a14ca8!2m2!1d144.8867587!2d-37.8562128?entry=ttu">(Map!)</a>
+        </h3>
+        <img src="images/Railway.png" class="center-img">
+        <br /><br /><br />
+
+        <h3 align="center">
+           Seaworks Maritime Museum and discovery centre <a href="https://www.google.com.au/maps/dir//Seaworks+Maritime+Precinct,+82%2F82+Nelson+Pl,+Williamstown+VIC+3016/@-37.8636749,144.9042692,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x6ad666f1f7c1ef77:0xb787f86e03926bc8!2m2!1d144.9068404!2d-37.8636833?entry=ttu">(Map!)</a>
+        </h3>
+        <img src="images/Seatime.png" class="center-img">
+    
+    </section>
+
+    <!--Accomodation section-->
+    <section>
+        <h1 align="center">Accomodation near beaches</h1>
+        <br /><br /><br />
+        <h2 align="center">St. kilda beach</h2>
+        <h3 align="center">1. The Prince Hotel</h3>
+        <img src="images/prince.png" class="center-img">
+        <br /><br /><br />
+        <h3 align="center">2. Novotel Melbourne</h3>
+        <img src="images/novotel.png" class="center-img">
+        <br /><br /><br />
+        <h3 align="center">3. Rydges St Kilda</h3>
+        <img src="images/rydges.png" class="center-img">
+        <br /><br /><br />
+        <h2 align="center">Brighton beach</h2>
+        <h3 align="center">1. Tolarno hotel</h3>
+        <img src="images/tolarno.png" class="center-img">
+        <br /><br /><br />
+        <h3 align="center">2. Quest Brighton on the Bay</h3>
+        <img src="images/quest.png" class="center-img">
+        <br /><br /><br />
+        <h3 align="center">3. Brighton Service Apartments</h3>
+        <img src="images/service.png" class="center-img">
+        <br /><br /><br />
+        <h2 align="center">Williamstown beach</h2>
+        <h3 align="center">1. Bayview Loft</h3>
+        <img src="images/bayview.png" class="center-img">
+        <br /><br /><br />
+        <h3 align="center">2. The Beach Bay Townhouse</h3>
+        <img src="images/bay.png" class="center-img">
+        <br /><br /><br />
+        <h3 align="center">3. Grandeur Innercity Townhouse</h3>
+        <img src="images/grandeur.png" class="center-img">
+        <br /><br /><br />
+
+
+        
+
+    
+     <!--Safety tips section-->
+     <section id="safety">
+        <div class="on">
+            <h1><pre> Safety Tips</pre></h1>
+            <h3><pre>
+                When heading to the beach, remember these essential safety tips for a worry-free experience: 
+                always swim in designated areas with lifeguards, stay hydrated, apply sunscreen regularly, 
+                be aware of local weather conditions, and keep an eye on children and non-swimmers. 
+                Additionally, respect beach rules and signs, avoid dangerous marine life, 
+                and never leave valuables unattended. Enjoy the sun, sand, and sea responsibly!
+            </pre>
+            </h3>
+            <div class="cnt">
+                <pre><a href="" >
+                Emergency Contact: 999
+                mail: Shahed@gmail.com
+                </a>
+                </pre>
+            </div>
+        </div>
+    </section>
+    
+    <!--Advice section-->
+    <section id="advice">
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="safety.css">
+            <title>Advice</title>
+        </head>
+        <body>
+            <div class="sh">
+                <h1><pre>Advice</pre></h1>
+                <h3><pre>
+                1. **Sun Protection**: Pack sunscreen with a high SPF, sunglasses, a wide-brimmed hat.
+                
+                2. **Hydration**: Bring plenty of water to stay hydrated, especially under the sun.
+                
+                3. **Beach Gear**: Don't forget your beach essentials like towels, umbrellas or sunshades.
+                
+                4. **Swimwear and Extra Clothing**: Pack your swimsuit, but also bring along a change of clothes.
+                
+                5. **Footwear**: Flip-flops or sandals are a must for sandy beaches.
+                
+                6. **Beach Safety**: Familiarize yourself with local beach safety guidelines.
+                
+                7. **First Aid Kit**: Pack a basic first aid kit with essentials.
+                
+                8. **Respect Nature**: Be mindful of your impact on the environment by disposing of trash properly.
+                
+                9. **Stay Alert**: Keep an eye on the water and weather conditions.
+                
+                10. **Know Your Limits**: Don't overexert yourself, especially if you're not a strong swimmer.
+                
+                11. **Stay Connected**: Keep your phone charged and handy for emergencies.
+                
+                12. **Food and Snacks**: Pack some snacks to keep you energized throughout the day.
+                
+                13. **Entertainment**: Bring along some beach-friendly entertainment like a good book or a beach ball.
+                
+                14. **Respect Local Rules and Customs**: Be aware of any specific rules at the beach you're visiting.
+                
+                15. **Enjoy Yourself**: Lastly, relax and enjoy the beauty of the beach! 
+                </pre></h3>
+            </div>
+            <br /><br /><br />
+    
+
+    <!--Packing list section-->
+    <section id="packing-list">
+        <div class="bm">
+            <h1><pre> List of Essentials:</pre></h1>
+            <h2><pre>
+                1.Umbrella
+                2.Camera
+                3.Power Bank
+                4.Surf Board
+                6.Snacks
+                7.Sun Glasses
+                8.Towels
+                9.Hat
+                10.Sunscreen
+            </pre></h2>
+        </div>
+    </section>
+
+
+
+    <!-- Checkout page section -->
+<main id="main">
+    <section id="left">
+        <div id="head">
+            <h1>Life has great moments</h1>
+            <p>Enjoy them with beach!</p>
+        </div>
+    </section>
+    <section id="right">
+        <h2 style="color: black; font-weight: bold; margin-bottom: 2px;">Purchase</h2>
+        <form action="#">
+            <!-- Card number field -->
+            <div id="form-card" class="form-field">
+                <label for="cc-number" style="color: black;">Card number:</label>
+                <input id="cc-number" maxlength="19" placeholder="1111 2222 3333 4444" required>
+            </div>
+
+            <!-- Expiry date field -->
+            <div id="form-date" class="form-field">
+                <label for="expiry-month" style="color: black;">Expiry date:</label>
+                <div id="date-val">
+                    <select id="expiry-month" required>
+                        <option id="trans-label_month" value="" default="default" selected="selected">Month</option>
+                        <option value="1">01</option>
+                        
+                    </select>
+                    <select id="expiry-year" required>
+                        <option id="trans-label_year" value="" default="" selected="selected">Year</option>
+                       
+                    </select>
+                </div>
+            </div>
+            
+            <!-- Security code field -->
+            <div id="form-sec-code" class="form-field">
+                <label for="sec-code" style="color: black;">Security code:</label>
+                <input type="password" maxlength="3" placeholder="123" required>
+            </div>
+            
+            <button type="submit">Purchase Trip</button>
+        </form>
+    </section>
+</main>
+<br /><br /><br />
+<section>
+  <h2>Sources / References</h2>
+  <p> Wikipedia, Google, and online databases in the library. </p>
+
+</section>
+<?php include 'footer.inc.php'; ?>
+</body>
+</html>
